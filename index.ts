4d26ed38bf5abfd117715580f0071ba53b7e4dd3
Parth <taksuparth@gmail.com>
2024-06-13 16:24:36 +0530

    chore: add websocket server

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import configure from './routers';
+import { WebSocket, WebSocketServer } from 'ws';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,6 +9,52 @@ configure(app);
 
 console.log(`Attempting to run server on port ${port}`);
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+const expressServer = app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+function onSocketPreError(error: Error) {
+  console.error('WebSocket pre error:', error);
+}
+
+function onSocketPostError(error: Error) {
+  console.error('WebSocket post error:', error);
+}
+
+const wss = new WebSocketServer({ noServer: true });
+
+wss.on('connection', (ws: WebSocket) => {
+  ws.on('error', onSocketPostError);
+  console.log('New WebSocket connection');
+
+  ws.on('message', (message: string, isBinary: boolean) => {
+    console.log(`Received message: ${message}`);
+
+    // Process the received message here
+    wss.clients.forEach(client => {
+      if (ws !== client && client.readyState === WebSocket.OPEN) {
+        client.send(message, { binary: isBinary });
+      }
+    });
+  });
+
+  ws.on('close', () => {
+    console.log('WebSocket connection closed');
+  });
+});
+
+expressServer.on('upgrade', (request, socket, head) => {
+  socket.on('error', onSocketPreError);
+
+  // perform authentication here
+  if (!!request.headers['authorization']) {
+    socket.write('HTTP/1.1 Unauthorized\r\n\r\n');
+    socket.destroy();
+    return;
+  }
+
+  wss.handleUpgrade(request, socket, head, ws => {
+    socket.removeListener('error', onSocketPreError);
+    wss.emit('connection', ws, request);
+  });
 });
